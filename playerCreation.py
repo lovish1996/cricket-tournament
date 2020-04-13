adb73e3c84035bca0dbd67455327f42d117febda
Vinay Kumar Agarwal <vka0797@gmail.com>
2020-04-14 03:25:53 +0530

    Changes for Player Data Script done

--- playerCreation.py
+++ playerCreation.py
@@ -1,24 +1,34 @@
 import os
 import sys
 import pandas as pd
+# Run the commans "python3 playerCreation.py <DataFileName>" from the project root directory for running the script
+
 import numpy as np
 import requests
 import json
 
-pathForData = "PlayerData.xlsx"
+
+host = "localhost"
+port= "8080"
+apiEndPoint = "/players/createPlayer"
 
 def isNaN(num):
     return num != num
 
-def generate():
+def generate(fileName):
+
+    pathForData = fileName
     playerData = pd.read_excel(pathForData)
+
+    # API to hit the post request
+    apiToHit = host + ":" + port + apiEndPoint
 
     for ind in range(playerData.shape[0]):
         playerName = playerData.iloc[ind,0]
         playerShirtId = int(playerData.iloc[ind,1])
         playerType = playerData.iloc[ind,2]
         positionOfResponsibility = playerData.iloc[ind,3]
-        team = playerData.iloc[ind,4]
+        teamName = playerData.iloc[ind,4]
 
         json_str={}
         if isNaN(positionOfResponsibility):
@@ -26,7 +36,7 @@ def generate():
                 "playerName" : playerName,
                 "playerShirtId" : playerShirtId,
                 "playerType" : playerType,
-                "team" : team
+                "teamName" : teamName
             }
         else:
             json_str = {
@@ -34,11 +44,12 @@ def generate():
                 "playerShirtId" : playerShirtId,
                 "playerType" : playerType,
                 "positionOfResponsibility" : positionOfResponsibility,
-                "team" : team
+                "teamName" : teamName
             }
-
-        command = "curl -X POST -H 'Content-type: application/json' --data '" + json.dumps(json_str) +  "' localhost:8080/players/createPlayer"
+            
+        command = "curl -X POST -H 'Content-type: application/json' --data '" + json.dumps(json_str) +  "' " + apiToHit
         os.system(command)
 
 if __name__=="__main__":
-    generate()
+    fileName = sys.argv[1]
+    generate(fileName)
